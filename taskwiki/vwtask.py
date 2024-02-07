a6111eba1dde69b82f8dabcd13264db3ca012fe0
Daniel L. Wilcox Jr. <danielwilcoxjr@gmail.com>
2024-02-07 01:54:03 -0600

    feature: allow completed checkboxes to be markdown style '- [x]' or vimwiki style '- [X]'

--- taskwiki/vwtask.py
+++ taskwiki/vwtask.py
@@ -110,7 +110,7 @@ class VimwikiTask(object):
                 len(match.group('priority') or [])) # This is either 0,1,2 or 3
 
             # Also make sure changes in the progress field are reflected
-            if self['completed_mark'] == 'X':
+            if self['completed_mark'] == 'x' or self['completed_mark'] == 'X':
                 self.task['status'] = 'completed'
                 self.task['start'] = None
                 self.task['end'] = self.task['end'] or datetime.now()
@@ -280,9 +280,14 @@ class VimwikiTask(object):
         mark = self['completed_mark']
 
         if self.task.completed:
-            mark = 'X'
-        elif mark == 'X':
-            mark = ' '
+            if self['completed_mark'] == 'x':
+                mark = 'x'
+            elif mark == 'x':
+                mark = ' '
+            elif self['completed_mark'] == 'X':
+                mark = 'X'
+            elif mark == 'X':
+                mark = ' '
 
         if self.task.active:
             mark = 'S'
